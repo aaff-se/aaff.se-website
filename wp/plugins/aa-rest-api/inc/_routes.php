d01e242036128e56bd3738b8e78c3e2f938fa1a7
Anton Andersson <anton@aaff.se>
2016-12-09 13:16:22 +0100

    changed the screenshot in the wp theme

--- wp/plugins/aa-rest-api/inc/_routes.php
+++ wp/plugins/aa-rest-api/inc/_routes.php
@@ -3,11 +3,11 @@
 // Initialise our routes
 add_action('rest_api_init', function() {
 	
-	
 	register_rest_route('aa/v1', 'global/footer', array(
 		'methods' => 'GET',
 		'callback' => 'aa_rest_footer',
 	));
+	
 	register_rest_route('aa/v1', 'home', array(
 		'methods' => 'GET',
 		'callback' => 'aa_rest_home',
